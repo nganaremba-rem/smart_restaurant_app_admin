ff244d6ad7e5d2931f4f9311646d7d034eec7663
Nganaremba <nganaremba-rem>
2024-04-30 17:30:42 +0530

    updated config endpoint

--- src/config.js
+++ src/config.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 export const Axios = axios.create({
-	baseURL: "http://localhost:5000/restaurant/api/v1/",
+	baseURL: "https://smart-restaurant-app-backend.vercel.app/",
 });
 
 // const Config = {
